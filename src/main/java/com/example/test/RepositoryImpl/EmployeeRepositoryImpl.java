e64f265815cfc7eb913b95fee5b56d48b3714552
KuryakovArtem <kurik01tagil@gmail.com>
2022-09-19 16:25:48 +0300

    cosmetic fix

--- src/main/java/com/example/test/RepositoryImpl/EmployeeRepositoryImpl.java
+++ src/main/java/com/example/test/RepositoryImpl/EmployeeRepositoryImpl.java
@@ -3,23 +3,13 @@ package com.example.test.RepositoryImpl;
 import com.example.test.DatabaseEntities.DatabaseDepartment;
 import com.example.test.DatabaseEntities.DatabaseEmployee;
 import com.example.test.DatabaseEntities.DatabasePerson;
-import com.example.test.Entities.Department;
 import com.example.test.Entities.Employee;
 import com.example.test.Enums.PersonType;
 import com.example.test.Repos.Custom.EmployeeRepositoryCustom;
 import com.example.test.Repos.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Repository;
-
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-import javax.persistence.Query;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.List;
 
 @Repository
 
